9666a9399e8ad52428016f4a00fd899efd667d1c
Ashwini kumar Singh <ashwinirajput1991@gmail.com>
2025-12-27 19:27:19 +0530

    qaw

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cors({
    origin: [
     "http://localhost:5173",
-    "https://your-frontend.vercel.app"
+    "https://logincodelmsfrontend.vercel.app"
   ],
   methods: ["GET", "POST"],
   credentials: true
